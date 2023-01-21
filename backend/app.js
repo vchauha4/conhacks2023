bf70cb17762cfaf43c325633baf151f72af15493
vchauha4 <60143557+vchauha4@users.noreply.github.com>
2023-01-21 17:07:40 -0500

    api fix

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@
 
 const express = require("express")
 const { ObjectId } = require("mongodb")
-const {connectToDb, getDB} = require('./db')
+const {connectToServer, getDb} = require('./db')
 
 
 //init app
@@ -12,11 +12,26 @@ const app = express()
 //db connection
 let db
 
-connectToDb((err) => {
+connectToServer((err) => {
     if(!err){
         app.listen(3001, ()=>{
             console.log('app listening on port 3001')
         })
-        db = getDB()
+        db = getDb()
     }
 })
+
+
+app.get('/category', (req,res)=> {
+
+  db
+    .collection("cpi")
+    .distinct('category',function (err, result) {
+        if (err) {
+          res.status(400).send("Error fetching listings!");
+       } else {
+          res.json(result);
+        }
+      });
+})
+
--- backend/db.js
+++ backend/db.js
@@ -3,18 +3,23 @@ const {MongoClient} = require('mongodb')
 let dbConnection
 let uri = "mongodb+srv://conuhacks:64lHWAwEeV1dDN0t@cluster0.pzf8u9p.mongodb.net/test"
 
+const client = new MongoClient(uri)
+
 module.exports = {
-    connectToDb: (cb) => {
-        MongoClient.connect(uri)
-            .then((client)=>{
-                dbConnection = client.db()
-                console.log("succesful connection")
-                return cb()
-            })
-            .catch(err=>{
-                console.log(err)
-                return cb(err)
-            })
-    },
-    getDB: () => dbConnection
+    connectToServer: function (callback) {
+        client.connect(function (err, db) {
+          if (err || !db) {
+            return callback(err);
+          }
+    
+          dbConnection = db.db("cpiDB");
+          console.log("Successfully connected to MongoDB.");
+    
+          return callback();
+        });
+      },
+    
+      getDb: function () {
+        return dbConnection;
+      },
 }
