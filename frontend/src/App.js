df28eb3054a9852dbe62f3c44a0a642b4a6f7389
palvisha13 <palvisha13@hotmail.com>
2023-01-22 03:35:06 -0500

    basic format for app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import { Grid, Typography } from '@mui/material';
+import { Grid, Typography, Card} from '@mui/material';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Typography sx={{marginLeft:"0"}}>
+        <Typography sx={{float:"left"}}>
           Budget Bucket
         </Typography>
       </header>
-      <Grid container sx={{height:"90vh", backgroundColor:"wheat", }}>
+      <Grid  sx={{height:"90vh", backgroundColor:"#d5d1d1", display:"grid", placeItems:"center" }}>
 
-        <Grid>
-          
-        </Grid>
+        <Card sx={{width:"50%", height:"100%", backgroundColor:"white", display:"flex", marginTop:"0%", marginBotom:"2%"}}>
+
+        </Card>
 
       </Grid>
     </div>
