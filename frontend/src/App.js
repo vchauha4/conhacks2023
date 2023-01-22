9a226cbf7e21fa00e467234ad751e16875c692d2
palvisha13 <palvisha13@hotmail.com>
2023-01-22 03:16:46 -0500

    wip app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Grid, Typography } from '@mui/material';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Typography sx={{marginLeft:"0"}}>
+          Budget Bucket
+        </Typography>
       </header>
+      <Grid container sx={{height:"90vh", backgroundColor:"wheat", }}>
+
+        <Grid>
+          
+        </Grid>
+
+      </Grid>
     </div>
   );
 }
