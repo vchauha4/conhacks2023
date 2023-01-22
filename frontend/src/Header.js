4cc082548f237dc0021ae51ba0e88b7a66f27745
vchauha4 <60143557+vchauha4@users.noreply.github.com>
2023-01-22 03:14:27 -0500

    header

---
+++ frontend/src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+import IconLabelButtons from './AddMGroupButton';
+import { Link } from 'react-router-dom';
+function Header() {
+  return (
+    <div style={{ backgroundColor: "rgb(81, 38, 130)", color: 'white', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+      <h1 style={{ marginLeft: '2.5%', cursor:'pointer' }}><Link to="/" style={{ textDecoration: 'none', color:'white' }}>findmyGC</Link></h1>
+      <SearchBar/>
+      <IconLabelButtons/>
+    </div>
+  );
+}
+
+export default Header;
