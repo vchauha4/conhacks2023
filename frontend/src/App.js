c7f02cb71470645659cbeaee5525a7985bcc97b2
Hisham632 <77436699+Hisham632@users.noreply.github.com>
2023-01-22 08:13:40 -0500

    init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,12 +6,21 @@ import GroceryList from './context/GroceryList.tsx';
 import { display } from '@mui/system';
 import {BudgetScore} from './context/BudgetScore';
 import SemiCircleProgressBar from "react-progressbar-semicircle";
+import React, { useEffect, useState } from "react";
 
 function App() {
+
+  const [percent, setPercent] = useState("50");
+
+  function updatingPercent(num){
+    setPercent(num)
+  }
+
+  
   return (
     <div className="App">
       <header className="App-header">
-        <Typography sx={{float:"left"}}>
+        <Typography sx={{float:"left",fontSize:36, color:'black',fontWeight:700   }}>
           Budget Bucket
         </Typography>
       </header>
@@ -20,9 +29,11 @@ function App() {
         <Card sx={{width:"50%", height:"100%", backgroundColor:"white", display:"flex", marginTop:"0%",display:"flex", marginBotom:"2%", flexDirection:"column"}}>
           <AddItemsPage></AddItemsPage>
           <Typography sx={{marginTop:"20%", alignItems:"left", justifyContent:"left"}}>Grocery List</Typography>
-          <GroceryList sx={{width:"100%", marginLecft:"10%"}}></GroceryList>
-          <Typography >Budget Score</Typography>
-          <SemiCircleProgressBar percentage={33} showPercentValue />
+          
+          <GroceryList  sx={{width:"100%", marginLecft:"10%"}}></GroceryList>
+
+          <Typography> Budget Score</Typography>
+          <SemiCircleProgressBar percentage={percent} showPercentValue />
 
         </Card>
         
--- frontend/src/pages/AddItemsPage.jsx
+++ frontend/src/pages/AddItemsPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Autocomplete, TextField, Grid } from '@mui/material'
 //import ControlPointIcon from '@mui/icons-material/ControlPoint';
 
@@ -6,6 +6,8 @@ import { useGrocerySelection } from '../context/GrocerySelectionContext'
 
 export default function AddItemsPage() {
   const { loaded, selected } = useGrocerySelection()
+
+  const {currentGroceries, setGroceries}=useState([])
 
 
   const demo = ["Apples","Bakery and cereal products (excluding baby food)","Bakery products","Bananas","Bread, rolls and buns","Breakfast cereal and other cereal products (excluding baby food)","Butter","Canned and other preserved fish","Canned vegetables and other vegetable preparations","Cereal products (excluding baby food)","Cheese","Coffee and tea","Condiments, spices and vinegars","Cookies and crackers","Dairy products","Dairy products and eggs","Edible fats and oils","Eggs","Fish","Fish, seafood and other marine products","Flour and flour-based mixes","Food purchased from fast food and take-out restaurants 5","Food purchased from restaurants 5","Food purchased from stores","Food purchased from table-service restaurants 5","Fresh fruit","Fresh milk","Fresh or frozen beef","Fresh or frozen chicken","Fresh or frozen fish (including portions and fish sticks)","Fresh or frozen meat (excluding poultry)","Fresh or frozen pork","Fresh or frozen poultry","Fresh vegetables","Frozen and dried vegetables","Fruit juices","Fruit, fruit preparations and nuts","Ham and bacon","Ice cream and related products","Lettuce","Meat","Non-alcoholic beverages","Nuts and seeds","Oranges","Other bakery products","Other food preparations","Other food products and non-alcoholic beverages","Other fresh fruit","Other fresh vegetables","Other preserved fruit and fruit preparations","Other processed meat","Pasta products","Potatoes","Preserved fruit and fruit preparations","Preserved vegetables and vegetable preparations","Processed meat","Rice and rice-based mixes","Sugar and confectionery","Tomatoes","Vegetables and vegetable preparations"]
@@ -19,7 +21,17 @@ export default function AddItemsPage() {
         sx={{display:"block", marginLeft:"auto"}}
         renderInput={
           (params)=><TextField {...params} />
+          
         }
+        onChange={(event, value) => {
+          console.log()
+
+
+
+          console.log(value)
+        
+        }} // prints the selected value
+
       />
       <div>
         {/* <ControlPointIcon fontSize='128' color="disabled" /> */}
